08962a442178eaa41f3570e4a19740b7e09245df
Zach Steindler <steiza@github.com>
2023-06-23 15:45:53 -0400

    Support `verify` command for conformance testing (#26)
    We do this by constructing a bundle and disabling bundle-specific verification options.

--- cmd/conformance/main.go
+++ cmd/conformance/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"os"
+
+	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	protocommon "github.com/sigstore/protobuf-specs/gen/pb-go/common/v1"
 
 	"github.com/github/sigstore-verifier/pkg/bundle"
 	"github.com/github/sigstore-verifier/pkg/policy"
@@ -11,11 +17,14 @@ import (
 )
 
 var bundlePath *string
+var certPath *string
 var certOIDC *string
 var certSAN *string
+var signaturePath *string
 
 func usage() {
 	fmt.Println("Usage:")
+	fmt.Printf("\t%s verify --signature FILE --certificate FILE --certificate-identity IDENTITY --certificate-oidc-issuer URL FILE\n", os.Args[0])
 	fmt.Printf("\t%s verify-bundle --bundle FILE --certificate-identity IDENTITY --certificate-oidc-issuer URL FILE\n", os.Args[0])
 }
 
@@ -25,7 +34,100 @@ func main() {
 		os.Exit(1)
 	}
 
-	if os.Args[1] == "verify-bundle" {
+	switch os.Args[1] {
+	case "verify":
+		for i := 2; i < len(os.Args); i += 2 {
+			switch os.Args[i] {
+			case "--certificate":
+				certPath = &os.Args[i+1]
+			case "--certificate-oidc-issuer":
+				certOIDC = &os.Args[i+1]
+			case "--certificate-identity":
+				certSAN = &os.Args[i+1]
+			case "--signature":
+				signaturePath = &os.Args[i+1]
+			}
+		}
+
+		// Load certificate
+		cert, err := os.ReadFile(*certPath)
+		if err != nil {
+			log.Fatalf("unable to open certificate file %s", *certPath)
+		}
+
+		pemCert, _ := pem.Decode(cert)
+		if pemCert == nil {
+			log.Fatalf("unable to load cerficate from %s", *certPath)
+		}
+
+		// Load signature
+		sig, err := os.ReadFile(*signaturePath)
+		if err != nil {
+			log.Fatalf("unable to open signature file %s", *signaturePath)
+		}
+		sigBytes, err := base64.StdEncoding.DecodeString(string(sig))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileBytes, err := os.ReadFile(os.Args[len(os.Args)-1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileDigest := sha256.Sum256(fileBytes)
+
+		// Construct bundle
+		signingCert := protocommon.X509Certificate{
+			RawBytes: pemCert.Bytes,
+		}
+
+		pb := protobundle.Bundle{
+			MediaType: bundle.SigstoreBundleMediaType01,
+			VerificationMaterial: &protobundle.VerificationMaterial{
+				Content: &protobundle.VerificationMaterial_X509CertificateChain{
+					X509CertificateChain: &protocommon.X509CertificateChain{
+						Certificates: []*protocommon.X509Certificate{&signingCert},
+					},
+				},
+			},
+			Content: &protobundle.Bundle_MessageSignature{
+				MessageSignature: &protocommon.MessageSignature{
+					MessageDigest: &protocommon.HashOutput{
+						Algorithm: protocommon.HashAlgorithm_SHA2_256,
+						Digest:    fileDigest[:],
+					},
+					Signature: sigBytes,
+				},
+			},
+		}
+
+		// Configure verification options
+		opts := policy.GetDefaultOptions()
+		opts.TlogOptions.Disable = true
+		opts.CtlogOptions.Disable = true
+		opts.TsaOptions.Disable = true
+
+		if *certOIDC != "" {
+			policy.SetExpectedOIDC(opts, *certOIDC)
+		}
+		if *certSAN != "" {
+			policy.SetExpectedSAN(opts, *certSAN)
+		}
+
+		// Load trust root
+		tr, err := root.NewTrustedRootFromPath("examples/trusted-root-public-good.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Verify bundle
+		p := policy.NewTrustedRootPolicy(tr, opts)
+		err = p.VerifyPolicy(bundle.NewProtobufBundle(&pb))
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "verify-bundle":
 		for i := 2; i < len(os.Args); i += 2 {
 			switch os.Args[i] {
 			case "--bundle":
@@ -77,7 +179,7 @@ func main() {
 		}
 
 		content.EnsureFileMatchesDigest(fileBytes)
-	} else {
+	default:
 		log.Fatalf("Unsupported command %s", os.Args[1])
 	}
 }
