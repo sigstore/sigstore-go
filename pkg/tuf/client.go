5a823fc2e6fbddbefacce83fe2e34cd0040736c9
Zach Steindler <steiza@github.com>
2023-08-18 09:32:18 -0400

    Update TUF client to use theupdateframework/go-tuf/client/filejsonstore (#49)
    Also add retries to HTTP requests

--- pkg/tuf/client.go
+++ pkg/tuf/client.go
@@ -4,18 +4,18 @@ import (
 	"crypto/sha512"
 	"embed"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
+	"path"
 
 	tufclient "github.com/theupdateframework/go-tuf/client"
+	filejsonstore "github.com/theupdateframework/go-tuf/client/filejsonstore"
 )
 
 //go:embed repository
 var embeddedRepos embed.FS
 
 const TrustedRootTUFPath = "trusted_root.json"
+const RootTUFPath = "root.json"
 
 type Writer struct {
 	Bytes []byte
@@ -30,17 +30,35 @@ func (w Writer) Delete() error {
 	return nil
 }
 
-type Signed struct {
-	Version int `json:"version"`
-}
-type RootMeta struct {
-	Signed Signed `json:"signed"`
-}
-
 func GetTrustedrootJSON(tufRootURL, workPath string) (trustedrootJSON []byte, err error) {
+	// Ensure we have a RootTUFPath file for this TUF URL
+	tufPath := path.Join(workPath, tufRootURL)
+
+	fileJSONStore, err := filejsonstore.NewFileJSONStore(tufPath)
+	if err != nil {
+		return nil, err
+	}
+
+	tufMetaMap, err := fileJSONStore.GetMeta()
+	if err != nil {
+		return nil, err
+	}
+
+	_, ok := tufMetaMap[RootTUFPath]
+	if !ok {
+		// There isn't a RootTUFPath for this TUF URL, so see if the library has one embedded
+		_, err = checkEmbedded(tufRootURL, fileJSONStore)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Now that we have fileJSONStore, create a tufclient and check remote for updates
 	tufRemoteOptions := &tufclient.HTTPRemoteOptions{
 		MetadataPath: "",
 		TargetsPath:  "targets",
+		Retries:      tufclient.DefaultHTTPRetries,
 	}
 
 	tufRemoteStore, err := tufclient.HTTPRemoteStore(fmt.Sprintf("https://%s", tufRootURL), tufRemoteOptions, nil)
@@ -48,97 +66,28 @@ func GetTrustedrootJSON(tufRootURL, workPath string) (trustedrootJSON []byte, er
 		return nil, err
 	}
 
-	tufLocalStore := tufclient.MemoryLocalStore()
-	var rootJSON json.RawMessage
-
-	tufPath := filepath.Join(workPath, tufRootURL)
-
-	err = os.MkdirAll(tufPath, 0755)
-	if err != nil {
-		return nil, err
-	}
-
-	rootJSONPath := filepath.Join(tufPath, "root.json")
-
-	if _, err := os.Stat(rootJSONPath); errors.Is(err, os.ErrNotExist) {
-		// Attempt to initialize the TUF repo
-		err := initialize(tufRootURL, workPath)
-		if err != nil {
-			return nil, fmt.Errorf("No root.json found at %s", tufPath)
-		}
-	}
-
-	root, err := os.ReadFile(rootJSONPath)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(root, &rootJSON)
-	if err != nil {
-		return nil, err
-	}
-	err = tufLocalStore.SetMeta("root.json", rootJSON)
-	if err != nil {
-		return nil, err
-	}
-
-	tufClient := tufclient.NewClient(tufLocalStore, tufRemoteStore)
-
-	oldMetaJSON, err := tufLocalStore.GetMeta()
-	if err != nil {
-		return nil, err
-	}
-
-	oldMeta := RootMeta{}
-	err = json.Unmarshal(oldMetaJSON["root.json"], &oldMeta)
-	if err != nil {
-		return nil, err
-	}
-
+	tufClient := tufclient.NewClient(fileJSONStore, tufRemoteStore)
 	targetFiles, err := tufClient.Update()
 	if err != nil {
 		return nil, err
 	}
 
-	// Check to see if there's a new root.json, and if so write it to disk
-	newMetaJSON, err := tufLocalStore.GetMeta()
-	if err != nil {
-		return nil, err
-	}
-
-	newMeta := RootMeta{}
-	err = json.Unmarshal(newMetaJSON["root.json"], &newMeta)
-	if err != nil {
-		return nil, err
-	}
-
-	if newMeta.Signed.Version > oldMeta.Signed.Version {
-		err = os.WriteFile(rootJSONPath, newMetaJSON["root.json"], 0600)
-		if err != nil {
-			return nil, err
-		}
-	}
-
+	// Now that we've updated, see if remote trustedroot metadata matches local disk
 	trustedrootMeta, ok := targetFiles[TrustedRootTUFPath]
 	if !ok {
 		return nil, fmt.Errorf("Unable to find %s via TUF", TrustedRootTUFPath)
 	}
 
-	trustedrootPath := filepath.Join(tufPath, TrustedRootTUFPath)
+	trustedroot, ok := tufMetaMap[TrustedRootTUFPath]
+	if ok {
+		hash := sha512.Sum512([]byte(trustedroot))
 
-	// See if trustedroot is on disk; if so check its hash against TUF
-	if _, err := os.Stat(trustedrootPath); !errors.Is(err, os.ErrNotExist) {
-		trustedroot, err := os.ReadFile(trustedrootPath)
-		if err == nil {
-			hash := sha512.Sum512([]byte(trustedroot))
-
-			if hash == [64]byte(trustedrootMeta.FileMeta.Hashes["sha512"]) {
-				return []byte(trustedroot), nil
-			}
+		if hash == [64]byte(trustedrootMeta.FileMeta.Hashes["sha512"]) {
+			return []byte(trustedroot), nil
 		}
 	}
 
-	// What's on disk didn't match, so download from TUF (and write to disk)
+	// What's on disk didn't match, so download from TUF remote (and cache it to disk)
 	writer := &Writer{
 		Bytes: make([]byte, 0),
 	}
@@ -148,7 +97,7 @@ func GetTrustedrootJSON(tufRootURL, workPath string) (trustedrootJSON []byte, er
 		return nil, err
 	}
 
-	err = os.WriteFile(trustedrootPath, writer.Bytes, 0600)
+	err = fileJSONStore.SetMeta(TrustedRootTUFPath, writer.Bytes)
 	if err != nil {
 		return nil, err
 	}
@@ -156,19 +105,18 @@ func GetTrustedrootJSON(tufRootURL, workPath string) (trustedrootJSON []byte, er
 	return writer.Bytes, nil
 }
 
-func initialize(tufRootURL, workPath string) error {
-	// Initialize the TUF repo by copying the embedded repo to disk
-	_, err := embeddedRepos.ReadDir(filepath.Join("repository", tufRootURL))
+func checkEmbedded(tufRootURL string, fileJSONStore *filejsonstore.FileJSONStore) (json.RawMessage, error) {
+	embeddedRootPath := path.Join("repository", tufRootURL, RootTUFPath)
+
+	root, err := embeddedRepos.ReadFile(embeddedRootPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	rootJSON, err := embeddedRepos.ReadFile(filepath.Join("repository", tufRootURL, "root.json"))
+
+	err = fileJSONStore.SetMeta(RootTUFPath, root)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	err = os.WriteFile(filepath.Join(workPath, tufRootURL, "root.json"), rootJSON, 0600)
-	if err != nil {
-		return err
-	}
-	return nil
+
+	return root, nil
 }
