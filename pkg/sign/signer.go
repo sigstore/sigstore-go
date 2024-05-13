7c6eb333752db42bb9fecc262f1f4abdd0683c99
Zach Steindler <steiza@github.com>
2024-05-13 11:21:00 -0400

    Add Rekor support to signing
    Signed-off-by: Zach Steindler <steiza@github.com>

--- pkg/sign/signer.go
+++ pkg/sign/signer.go
@@ -15,13 +15,31 @@
 package sign
 
 import (
+	"encoding/pem"
+
 	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
 	protocommon "github.com/sigstore/protobuf-specs/gen/pb-go/common/v1"
 )
 
 const bundleV03MediaType = "application/vnd.dev.sigstore.bundle.v0.3+json"
 
-func Bundle(content Content, keypair Keypair, fulcio *Fulcio, idToken string, timestampAuthority *TimestampAuthority) (*protobundle.Bundle, error) {
+type BundleOptions struct {
+	// Optional Fulcio instance to get code signing certificate from.
+	//
+	// Resulting bundle will contain a certificate for its verification
+	// material content, instead of a public key.
+	Fulcio *Fulcio
+	// Optional OIDC JWT to send to Fulcio; required if using Fulcio
+	IDToken string
+	// Optional list of timestamp authorities to contact for inclusion in bundle
+	TimestampAuthorities []*TimestampAuthority
+	// Optional Rekor instance to get transparency log entry from.
+	//
+	// Supports hashedrekord and dsse entry types
+	Rekor *Rekor
+}
+
+func Bundle(content Content, keypair Keypair, opts BundleOptions) (*protobundle.Bundle, error) {
 	bundle := &protobundle.Bundle{MediaType: bundleV03MediaType}
 
 	// Sign content and add to bundle
@@ -33,8 +51,9 @@ func Bundle(content Content, keypair Keypair, fulcio *Fulcio, idToken string, ti
 	content.Bundle(bundle, signature, digest, keypair.GetHashAlgorithm())
 
 	// Add verification information to bundle
-	if fulcio != nil {
-		certBytes, err := fulcio.GetCertificate(keypair, idToken)
+	var pubKeyPEM []byte
+	if opts.Fulcio != nil && opts.IDToken != "" {
+		pubKeyBytes, err := opts.Fulcio.GetCertificate(keypair, opts.IDToken)
 		if err != nil {
 			return nil, err
 		}
@@ -42,10 +61,15 @@ func Bundle(content Content, keypair Keypair, fulcio *Fulcio, idToken string, ti
 		bundle.VerificationMaterial = &protobundle.VerificationMaterial{
 			Content: &protobundle.VerificationMaterial_Certificate{
 				Certificate: &protocommon.X509Certificate{
-					RawBytes: certBytes,
+					RawBytes: pubKeyBytes,
 				},
 			},
 		}
+
+		pubKeyPEM = pem.EncodeToMemory(&pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: pubKeyBytes,
+		})
 
 		// TODO: do verification of Fulcio certificate
 	} else {
@@ -56,9 +80,15 @@ func Bundle(content Content, keypair Keypair, fulcio *Fulcio, idToken string, ti
 				},
 			},
 		}
+
+		pubKeyStr, err := keypair.GetPublicKeyPem()
+		if err != nil {
+			return nil, err
+		}
+		pubKeyPEM = []byte(pubKeyStr)
 	}
 
-	if timestampAuthority != nil {
+	for _, timestampAuthority := range opts.TimestampAuthorities {
 		timestampBytes, err := timestampAuthority.GetTimestamp(signature)
 		if err != nil {
 			return nil, err
@@ -68,10 +98,18 @@ func Bundle(content Content, keypair Keypair, fulcio *Fulcio, idToken string, ti
 			SignedTimestamp: timestampBytes,
 		}
 
-		tsVerificationData := &protobundle.TimestampVerificationData{}
-		tsVerificationData.Rfc3161Timestamps = append(tsVerificationData.Rfc3161Timestamps, signedTimestamp)
+		if bundle.VerificationMaterial.TimestampVerificationData == nil {
+			bundle.VerificationMaterial.TimestampVerificationData = &protobundle.TimestampVerificationData{}
+		}
 
-		bundle.VerificationMaterial.TimestampVerificationData = tsVerificationData
+		bundle.VerificationMaterial.TimestampVerificationData.Rfc3161Timestamps = append(bundle.VerificationMaterial.TimestampVerificationData.Rfc3161Timestamps, signedTimestamp)
+	}
+
+	if opts.Rekor != nil {
+		err = opts.Rekor.GetTransparencyLog(pubKeyPEM, bundle)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return bundle, nil
---
+++ pkg/sign/transparency.go
@@ -0,0 +1,198 @@
+// Copyright 2024 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sign
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"time"
+
+	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	protocommon "github.com/sigstore/protobuf-specs/gen/pb-go/common/v1"
+	protorekor "github.com/sigstore/protobuf-specs/gen/pb-go/rekor/v1"
+	"github.com/sigstore/rekor/pkg/client"
+	"github.com/sigstore/rekor/pkg/generated/client/entries"
+	"github.com/sigstore/rekor/pkg/generated/models"
+	"github.com/sigstore/rekor/pkg/pki"
+	"github.com/sigstore/rekor/pkg/types"
+	"github.com/sigstore/rekor/pkg/types/dsse"
+	"github.com/sigstore/rekor/pkg/types/hashedrekord"
+	"github.com/sigstore/rekor/pkg/util"
+
+	// To initialize rekor types
+	_ "github.com/sigstore/rekor/pkg/types/dsse/v0.0.1"
+	_ "github.com/sigstore/rekor/pkg/types/hashedrekord/v0.0.1"
+)
+
+type Transparency interface {
+	GetTransparencyLog([]byte, *protobundle.Bundle) error
+}
+
+type Rekor struct {
+	options *RekorOptions
+}
+
+type RekorOptions struct {
+	// URL of Fulcio instance
+	BaseURL string
+	// Optional timeout for network requests
+	Timeout time.Duration
+	// Optional version string for user agent
+	LibraryVersion string
+}
+
+func NewRekor(opts *RekorOptions) *Rekor {
+	return &Rekor{options: opts}
+}
+
+func (r *Rekor) GetTransparencyLog(pubKeyPEM []byte, b *protobundle.Bundle) error {
+	artifactProperties := types.ArtifactProperties{
+		PublicKeyBytes: [][]byte{pubKeyPEM},
+	}
+
+	dsseEnvelope := b.GetDsseEnvelope()
+	messageSignature := b.GetMessageSignature()
+	verificationMaterial := b.GetVerificationMaterial()
+	bundleCertificate := verificationMaterial.GetCertificate()
+
+	var proposedEntry models.ProposedEntry
+	var kind, version string
+
+	switch {
+	case dsseEnvelope != nil:
+		dsseType := dsse.New()
+		kind = dsse.KIND
+		version = dsseType.DefaultVersion()
+
+		artifactBytes, err := json.Marshal(dsseEnvelope)
+		if err != nil {
+			return err
+		}
+
+		artifactProperties.ArtifactBytes = artifactBytes
+
+		proposedEntry, err = dsseType.CreateProposedEntry(context.TODO(), "", artifactProperties)
+		if err != nil {
+			return err
+		}
+	case messageSignature != nil:
+		hashedrekordType := hashedrekord.New()
+		kind = hashedrekord.KIND
+		version = hashedrekordType.DefaultVersion()
+
+		if bundleCertificate == nil {
+			return errors.New("hashedrekord requires X.509 certificate")
+		}
+
+		hexDigest := hex.EncodeToString(messageSignature.MessageDigest.Digest)
+
+		artifactProperties.PKIFormat = string(pki.X509)
+		artifactProperties.SignatureBytes = messageSignature.Signature
+		artifactProperties.ArtifactHash = util.PrefixSHA(hexDigest)
+
+		var err error
+		proposedEntry, err = hashedrekordType.CreateProposedEntry(context.TODO(), "", artifactProperties)
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New("unable to find signature in bundle")
+	}
+
+	params := entries.NewCreateLogEntryParams()
+	if r.options.Timeout > 0 {
+		params.SetTimeout(r.options.Timeout)
+	}
+	params.SetProposedEntry(proposedEntry)
+
+	client, err := client.GetRekorClient(r.options.BaseURL, client.WithUserAgent(constructUserAgent(r.options.LibraryVersion)))
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Entries.CreateLogEntry(params)
+	if err != nil {
+		return err
+	}
+
+	entry := resp.Payload[resp.ETag]
+	keyID, err := hex.DecodeString(*entry.LogID)
+	if err != nil {
+		return err
+	}
+
+	set, err := base64.StdEncoding.DecodeString(entry.Verification.SignedEntryTimestamp.String())
+	if err != nil {
+		return err
+	}
+
+	inclusionProof := entry.Verification.InclusionProof
+	rootHash, err := hex.DecodeString(*inclusionProof.RootHash)
+	if err != nil {
+		return err
+	}
+
+	hashes := make([][]byte, len(inclusionProof.Hashes))
+	for i := 0; i < len(inclusionProof.Hashes); i++ {
+		eachHash, err := hex.DecodeString(inclusionProof.Hashes[i])
+		if err != nil {
+			return err
+		}
+		hashes[i] = eachHash
+	}
+
+	body, ok := entry.Body.(string)
+	if !ok {
+		return errors.New("unable to understand rekor body")
+	}
+	canonicalizedBody, err := base64.StdEncoding.DecodeString(body)
+	if err != nil {
+		return err
+	}
+
+	tlogEntries := []*protorekor.TransparencyLogEntry{
+		{
+			LogIndex: *entry.LogIndex,
+			LogId: &protocommon.LogId{
+				KeyId: keyID,
+			},
+			KindVersion: &protorekor.KindVersion{
+				Kind:    kind,
+				Version: version,
+			},
+			IntegratedTime: *entry.IntegratedTime,
+			InclusionPromise: &protorekor.InclusionPromise{
+				SignedEntryTimestamp: set,
+			},
+			InclusionProof: &protorekor.InclusionProof{
+				LogIndex: *inclusionProof.LogIndex,
+				RootHash: rootHash,
+				TreeSize: *inclusionProof.TreeSize,
+				Hashes:   hashes,
+				Checkpoint: &protorekor.Checkpoint{
+					Envelope: *inclusionProof.Checkpoint,
+				},
+			},
+			CanonicalizedBody: canonicalizedBody,
+		},
+	}
+
+	b.VerificationMaterial.TlogEntries = tlogEntries
+
+	return nil
+}
