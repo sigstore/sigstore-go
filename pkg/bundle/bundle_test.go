04843b55b400770fa02072d65ecba52ef9e6dbb7
Colleen Murphy <cmurphy@users.noreply.github.com>
2024-06-26 18:05:30 -0700

    Add unit tests for bundle package (#217)
    * Add tests for validate() in bundle package
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Add tests for VerificationContent in bundle pkg
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Add tests for SignatureContent and Envelope
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    * Add tests for Timestamps in bundle package
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>
    
    ---------
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

--- pkg/bundle/bundle_test.go
+++ pkg/bundle/bundle_test.go
@@ -15,9 +15,18 @@
 package bundle
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
 	"fmt"
+	"math/big"
 	"testing"
 
+	protobundle "github.com/sigstore/protobuf-specs/gen/pb-go/bundle/v1"
+	protocommon "github.com/sigstore/protobuf-specs/gen/pb-go/common/v1"
+	rekorv1 "github.com/sigstore/protobuf-specs/gen/pb-go/rekor/v1"
+	_ "github.com/sigstore/rekor/pkg/types/hashedrekord"
 	"github.com/stretchr/testify/require"
 )
 
@@ -121,6 +130,589 @@ func TestMediaTypeString(t *testing.T) {
 			}
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, res)
+		})
+	}
+}
+
+func Test_validate(t *testing.T) {
+	t.Parallel()
+	tlogBody := map[string]any{
+		"kind":       "hashedrekord",
+		"apiVersion": "0.0.1",
+		"spec": map[string]any{
+			"signature": map[string]any{
+				"content": "sn/VqLMqWjDeYt93XTb6LzWIsKIn5bOvEsZQyF1elkvpur85LoDk5q/ExGWBB0Y+v8q0B04Bg2xGMOVMNyD/LQ==",
+				"publicKey": map[string]any{
+					"content": "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUJnekNDQVMyZ0F3SUJBZ0lVS2cxZHN1OTBoS0daVW5WN1RRWFZPRjdOZCtrd0RRWUpLb1pJaHZjTkFRRUwKQlFBd0ZqRVVNQklHQTFVRUF3d0xhblZ6ZEhSeWRYTjBiV1V3SGhjTk1qUXdOakkwTWpJMU5USXpXaGNOTXpRdwpOakl5TWpJMU5USXpXakFXTVJRd0VnWURWUVFEREF0cWRYTjBkSEoxYzNSdFpUQmNNQTBHQ1NxR1NJYjNEUUVCCkFRVUFBMHNBTUVnQ1FRRGIwNjhSMkpYNStZSE5nZWVyeDlzM1k2eEp2ZVdPRGl3YnROZWtKaytTWUlDUjNYQlQKaDErNUJ1SStwTGNyTXNyQTZlOThaNkNxUkJjNDdEL05LdWgvQWdNQkFBR2pVekJSTUIwR0ExVWREZ1FXQkJTbgpKbExuNWZjeXYzNnlibHBKYTVkcmdhQlNBREFmQmdOVkhTTUVHREFXZ0JTbkpsTG41ZmN5djM2eWJscEphNWRyCmdhQlNBREFQQmdOVkhSTUJBZjhFQlRBREFRSC9NQTBHQ1NxR1NJYjNEUUVCQ3dVQUEwRUFaaTNCMTF4VDY5TjQKNnl4ODg5Rkl2Z0xIdjQvaUROR2JTUkpHanlXMXY1RFpscXBBT0dYWjc5V3d2TFJZQlAxbFhid0tGaGlzTlNsUwpNRk84c0FHZ1hRPT0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=",
+				},
+			},
+			"data": map[string]any{
+				"hash": map[string]any{
+					"algorithm": "sha256",
+					"value":     "bc103b4a84971ef6459b294a2b98568a2bfb72cded09d4acd1e16366a401f95b",
+				},
+			},
+		},
+	}
+	canonicalTlogBody, err := json.Marshal(tlogBody)
+	require.NoError(t, err)
+	tests := []struct {
+		name    string
+		pb      ProtobufBundle
+		wantErr bool
+	}{
+		{
+			name: "invalid media type",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "version too low",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle.v0.0.1+json",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "version too high",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.4",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "no verification material",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.1",
+				},
+			},
+		},
+		{
+			name: "v0.1 with no inclusion promise",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.1",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "v0.1 with inclusion promise",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.1",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+								InclusionPromise: &rekorv1.InclusionPromise{
+									SignedEntryTimestamp: []byte("1"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "v0.2 with no inclusion proof",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.2",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "v0.2 with inclusion proof",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.2",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+								InclusionProof: &rekorv1.InclusionProof{
+									LogIndex:   42,
+									RootHash:   []byte("b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"),
+									Checkpoint: &rekorv1.Checkpoint{},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "v0.3 with x.509 certificate chain",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.3",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+								InclusionProof: &rekorv1.InclusionProof{
+									LogIndex:   42,
+									RootHash:   []byte("b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"),
+									Checkpoint: &rekorv1.Checkpoint{},
+								},
+							},
+						},
+						Content: &protobundle.VerificationMaterial_X509CertificateChain{
+							X509CertificateChain: &protocommon.X509CertificateChain{},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "v0.3 without x.509 certificate chain",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					MediaType: "application/vnd.dev.sigstore.bundle+json;version=0.3",
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TlogEntries: []*rekorv1.TransparencyLogEntry{
+							{
+								LogIndex: 42,
+								LogId: &protocommon.LogId{
+									KeyId: []byte("deadbeef"),
+								},
+								KindVersion: &rekorv1.KindVersion{
+									Kind:    "hashedrekord",
+									Version: "0.0.1",
+								},
+								IntegratedTime:    1,
+								CanonicalizedBody: canonicalTlogBody,
+								InclusionProof: &rekorv1.InclusionProof{
+									LogIndex:   42,
+									RootHash:   []byte("b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"),
+									Checkpoint: &rekorv1.Checkpoint{},
+								},
+							},
+						},
+						Content: &protobundle.VerificationMaterial_Certificate{
+							Certificate: &protocommon.X509Certificate{},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.pb.validate()
+			if (got != nil) != tt.wantErr {
+				t.Errorf("validate() error = %v, wantErr = %v", got, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestVerificationContent(t *testing.T) {
+	t.Parallel()
+	caCert := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+	}
+	leafCert := &x509.Certificate{
+		SerialNumber: big.NewInt(2),
+	}
+	caKey, err := rsa.GenerateKey(rand.Reader, 512) //nolint:gosec
+	require.NoError(t, err)
+	leafKey, err := rsa.GenerateKey(rand.Reader, 512) //nolint:gosec
+	require.NoError(t, err)
+	caDer, err := x509.CreateCertificate(rand.Reader, caCert, caCert, &caKey.PublicKey, caKey)
+	require.NoError(t, err)
+	leafDer, err := x509.CreateCertificate(rand.Reader, leafCert, caCert, &leafKey.PublicKey, caKey)
+	require.NoError(t, err)
+	tests := []struct {
+		name            string
+		pb              ProtobufBundle
+		wantCertificate bool
+		wantPublicKey   bool
+		wantErr         bool
+	}{
+		{
+			name: "no verification material",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "certificate chain with zero certs",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_X509CertificateChain{
+							X509CertificateChain: &protocommon.X509CertificateChain{},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "certificate chain with self-signed cert",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_X509CertificateChain{
+							X509CertificateChain: &protocommon.X509CertificateChain{
+								Certificates: []*protocommon.X509Certificate{
+									{
+										RawBytes: caDer,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantCertificate: true,
+		},
+		{
+			name: "certificate chain",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_X509CertificateChain{
+							X509CertificateChain: &protocommon.X509CertificateChain{
+								Certificates: []*protocommon.X509Certificate{
+									{
+										RawBytes: leafDer,
+									},
+									{
+										RawBytes: caDer,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantCertificate: true,
+		},
+		{
+			name: "certificate chain with invalid cert",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_X509CertificateChain{
+							X509CertificateChain: &protocommon.X509CertificateChain{
+								Certificates: []*protocommon.X509Certificate{
+									{
+										RawBytes: []byte("hello"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "certificate",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_Certificate{
+							Certificate: &protocommon.X509Certificate{
+								RawBytes: leafDer,
+							},
+						},
+					},
+				},
+			},
+			wantCertificate: true,
+		},
+		{
+			name: "invalid certificate",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_Certificate{
+							Certificate: &protocommon.X509Certificate{
+								RawBytes: []byte("hello"),
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "public key",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						Content: &protobundle.VerificationMaterial_PublicKey{
+							PublicKey: &protocommon.PublicKeyIdentifier{},
+						},
+					},
+				},
+			},
+			wantPublicKey: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := tt.pb.VerificationContent()
+			if tt.wantErr {
+				require.Error(t, gotErr)
+				return
+			}
+			require.NoError(t, gotErr)
+			if tt.wantCertificate {
+				require.NotNil(t, got.GetCertificate())
+				return
+			}
+			if tt.wantPublicKey {
+				_, hasPubKey := got.HasPublicKey()
+				require.True(t, hasPubKey)
+				return
+			}
+		})
+	}
+}
+
+func TestSignatureContent(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name          string
+		pb            ProtobufBundle
+		wantEnvelope  bool
+		wantSignature bool
+	}{
+		{
+			name: "dsse envelope",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					Content: &protobundle.Bundle_DsseEnvelope{},
+				},
+			},
+			wantEnvelope: true,
+		},
+		{
+			name: "message signature",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					Content: &protobundle.Bundle_MessageSignature{
+						MessageSignature: &protocommon.MessageSignature{
+							MessageDigest: &protocommon.HashOutput{},
+						},
+					},
+				},
+			},
+			wantSignature: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := tt.pb.SignatureContent()
+			require.NoError(t, gotErr)
+			if tt.wantEnvelope {
+				require.NotNil(t, got.EnvelopeContent())
+				return
+			}
+			if tt.wantSignature {
+				require.NotNil(t, got.MessageSignatureContent())
+				return
+			}
+		})
+	}
+}
+
+func TestEnvelope(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		pb      ProtobufBundle
+		wantErr bool
+	}{
+		{
+			name: "dsse envelope",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					Content: &protobundle.Bundle_DsseEnvelope{},
+				},
+			},
+		},
+		{
+			name: "message signature",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					Content: &protobundle.Bundle_MessageSignature{
+						MessageSignature: &protocommon.MessageSignature{
+							MessageDigest: &protocommon.HashOutput{},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			_, gotErr := tt.pb.Envelope()
+			if tt.wantErr {
+				require.Error(t, gotErr)
+				return
+			}
+			require.NoError(t, gotErr)
+		})
+	}
+}
+
+func TestTimestamps(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name           string
+		pb             ProtobufBundle
+		wantTimestamps [][]byte
+		wantErr        bool
+	}{
+		{
+			name:    "missing verification material",
+			pb:      ProtobufBundle{Bundle: &protobundle.Bundle{}},
+			wantErr: true,
+		},
+		{
+			name: "empty timestamp data",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{},
+				},
+			},
+			wantTimestamps: make([][]byte, 0),
+		},
+		{
+			name: "one timestamp",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TimestampVerificationData: &protobundle.TimestampVerificationData{
+							Rfc3161Timestamps: []*protocommon.RFC3161SignedTimestamp{
+								{
+									SignedTimestamp: []byte("sometime yesterday"),
+								},
+							},
+						},
+					},
+				},
+			},
+			wantTimestamps: [][]byte{
+				[]byte("sometime yesterday"),
+			},
+		},
+		{
+			name: "multiple timestamps",
+			pb: ProtobufBundle{
+				Bundle: &protobundle.Bundle{
+					VerificationMaterial: &protobundle.VerificationMaterial{
+						TimestampVerificationData: &protobundle.TimestampVerificationData{
+							Rfc3161Timestamps: []*protocommon.RFC3161SignedTimestamp{
+								{
+									SignedTimestamp: []byte("sometime yesterday"),
+								},
+								{
+									SignedTimestamp: []byte("last week"),
+								},
+							},
+						},
+					},
+				},
+			},
+			wantTimestamps: [][]byte{
+				[]byte("sometime yesterday"),
+				[]byte("last week"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := tt.pb.Timestamps()
+			if tt.wantErr {
+				require.Error(t, gotErr)
+				return
+			}
+			require.NoError(t, gotErr)
+			require.Equal(t, tt.wantTimestamps, got)
 		})
 	}
 }
