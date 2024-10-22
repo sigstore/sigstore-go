b394b763880d3701a30c33bf9c63c0219d0b79de
Dmitry S <dsavints@gmail.com>
2024-10-22 13:17:38 +0200

    expand oci-image-verification example for private infra
    Signed-off-by: Dmitry S <dsavints@gmail.com>

--- examples/sigstore-go-signing/main.go
+++ examples/sigstore-go-signing/main.go
@@ -21,13 +21,12 @@ import (
 	"os"
 	"time"
 
-	"github.com/theupdateframework/go-tuf/v2/metadata/fetcher"
-	"google.golang.org/protobuf/encoding/protojson"
-
 	"github.com/sigstore/sigstore-go/pkg/root"
 	"github.com/sigstore/sigstore-go/pkg/sign"
 	"github.com/sigstore/sigstore-go/pkg/tuf"
 	"github.com/sigstore/sigstore-go/pkg/util"
+	"github.com/theupdateframework/go-tuf/v2/metadata/fetcher"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 var idToken *string
