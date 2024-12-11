79aa154dbdbb794e0f086176aa6f0bbd21609ba5
Cody Soyland <codysoyland@github.com>
2024-12-11 16:39:06 -0500

    Add certificate-revocation example
    This adds an example of how to use a custom TrustedMaterial to implement a Certificate Revocation List
    
    Signed-off-by: Cody Soyland <codysoyland@github.com>

---
+++ examples/certificate-revocation/crl_test.go
@@ -0,0 +1,42 @@
+// Copyright 2024 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/sigstore/sigstore-go/pkg/testing/ca"
+	"github.com/sigstore/sigstore-go/pkg/verify"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCRLWrapper(t *testing.T) {
+	virtualSigstore, err := ca.NewVirtualSigstore()
+	assert.NoError(t, err)
+
+	leaf, _, err := virtualSigstore.GenerateLeafCert("example@example.com", "issuer")
+	assert.NoError(t, err)
+
+	trustedMaterial := NewBasicCRLTrustedMaterial(virtualSigstore, []*big.Int{})
+	trustedMaterialWithRevocation := NewBasicCRLTrustedMaterial(virtualSigstore, []*big.Int{leaf.SerialNumber})
+
+	_, err = verify.VerifyLeafCertificate(time.Now(), leaf, trustedMaterial)
+	assert.NoError(t, err)
+
+	_, err = verify.VerifyLeafCertificate(time.Now(), leaf, trustedMaterialWithRevocation)
+	assert.Error(t, err)
+}
