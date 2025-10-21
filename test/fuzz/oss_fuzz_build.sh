0c698bb0eca0e8dc0c4d164d24ed507e81fa15d7
Adam Korczynski <adam@adalogics.com>
2025-10-21 13:47:23 +0100

    upgrade OSS-Fuzz build tool
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- test/fuzz/oss_fuzz_build.sh
+++ test/fuzz/oss_fuzz_build.sh
@@ -13,18 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-go get github.com/AdamKorcz/go-118-fuzz-build/testing
-
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/bundle FuzzBundle FuzzBundle
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/tlog FuzzParseEntry FuzzParseEntry
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/tlog FuzzParseEntry FuzzParseEntry
+sed -i 's/package verify_test/package verify/g' ./pkg/verify/fuzz_test.go
 mkdir pkg/verify/fuzz && mv pkg/verify/fuzz_test.go pkg/verify/fuzz/
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignedTimestampWithoutThreshold FuzzVerifySignedTimestampWithoutThreshold
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignedTimestampWithThreshold FuzzVerifySignedTimestampWithThreshold
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifyTlogEntry FuzzVerifyTlogEntry
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifier FuzzVerifier
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithoutArtifactOrDigest FuzzVerifySignatureWithoutArtifactOrDigest
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithArtifactWithoutDigest FuzzVerifySignatureWithArtifactWithoutDigest
-compile_native_go_fuzzer github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithArtifactDigest FuzzVerifySignatureWithArtifactDigest
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignedTimestampWithoutThreshold FuzzVerifySignedTimestampWithoutThreshold
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignedTimestampWithThreshold FuzzVerifySignedTimestampWithThreshold
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifyTlogEntry FuzzVerifyTlogEntry
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifier FuzzVerifier
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithoutArtifactOrDigest FuzzVerifySignatureWithoutArtifactOrDigest
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithArtifactWithoutDigest FuzzVerifySignatureWithArtifactWithoutDigest
+compile_native_go_fuzzer_v2 github.com/sigstore/sigstore-go/pkg/verify/fuzz FuzzVerifySignatureWithArtifactDigest FuzzVerifySignatureWithArtifactDigest
 
 zip -j $OUT/FuzzVerifier_seed_corpus.zip examples/trusted-root-public-good.json
 
